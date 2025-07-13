51a24c20abbf2cda88467d46b90108ffeec9a38f
bjowb <pulkt@gmail.com>
2025-07-13 14:12:10 +0530

    ECHO flag removal.

--- gun.c
+++ gun.c
@@ -1,7 +1,34 @@
+#include <stdlib.h>
 #include <unistd.h>
+#include <termios.h>
+
+
+
+
+struct termios orig;
+
+void disableRawMode(){
+    tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig);
+}
+
+void enableRawMode(){
+    
+    tcgetattr(STDIN_FILENO,&orig);
+    atexit(disableRawMode);
+
+
+    struct termios raw = orig;
+    
+    raw.c_cflag &= ~(ECHO);
+
+    tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
+}
 
 
 int main(){
+
+    enableRawMode();
+
     char c;
     while(read(STDIN_FILENO,&c,1) == 1 && c != 'q');
     return 0;
